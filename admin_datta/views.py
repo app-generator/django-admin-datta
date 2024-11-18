2c728eba1660e7cdeecb986f6fea1294455182d3
mominur-helios <mominur.rahman@heliosinsider.com>
2024-11-18 17:14:08 +0600

    Upgrade to new theme

--- admin_datta/views.py
+++ admin_datta/views.py
@@ -8,138 +8,67 @@ from django.contrib.auth.decorators import login_required
 
 def index(request):
   context = {
-    'segment': 'index'
+    'segment': 'dashboard'
   }
   return render(request, "pages/index.html", context)
 
 # Components
-@login_required(login_url='/accounts/login/')
-def bc_button(request):
+def color(request):
   context = {
-    'parent': 'basic_components',
-    'segment': 'button'
+    'segment': 'color'
   }
-  return render(request, "pages/components/bc_button.html", context)
+  return render(request, "pages/color.html", context)
 
-@login_required(login_url='/accounts/login/')
-def bc_badges(request):
+def typography(request):
   context = {
-    'parent': 'basic_components',
-    'segment': 'badges'
-  }
-  return render(request, "pages/components/bc_badges.html", context)
-
-@login_required(login_url='/accounts/login/')
-def bc_breadcrumb_pagination(request):
-  context = {
-    'parent': 'basic_components',
-    'segment': 'breadcrumbs_&_pagination'
-  }
-  return render(request, "pages/components/bc_breadcrumb-pagination.html", context)
-
-@login_required(login_url='/accounts/login/')
-def bc_collapse(request):
-  context = {
-    'parent': 'basic_components',
-    'segment': 'collapse'
-  }
-  return render(request, "pages/components/bc_collapse.html", context)
-
-@login_required(login_url='/accounts/login/')
-def bc_tabs(request):
-  context = {
-    'parent': 'basic_components',
-    'segment': 'navs_&_tabs'
-  }
-  return render(request, "pages/components/bc_tabs.html", context)
-
-@login_required(login_url='/accounts/login/')
-def bc_typography(request):
-  context = {
-    'parent': 'basic_components',
     'segment': 'typography'
   }
-  return render(request, "pages/components/bc_typography.html", context)
+  return render(request, "pages/typography.html", context)
 
-@login_required(login_url='/accounts/login/')
 def icon_feather(request):
   context = {
-    'parent': 'basic_components',
     'segment': 'feather_icon'
   }
-  return render(request, "pages/components/icon-feather.html", context)
+  return render(request, "pages/icon-feather.html", context)
 
-
-# Forms and Tables
-@login_required(login_url='/accounts/login/')
-def form_elements(request):
-  context = {
-    'parent': 'form_components',
-    'segment': 'form_elements'
-  }
-  return render(request, 'pages/form_elements.html', context)
-
-@login_required(login_url='/accounts/login/')
-def basic_tables(request):
-  context = {
-    'parent': 'tables',
-    'segment': 'basic_tables'
-  }
-  return render(request, 'pages/tbl_bootstrap.html', context)
-
-# Chart and Maps
-@login_required(login_url='/accounts/login/')
-def morris_chart(request):
-  context = {
-    'parent': 'chart',
-    'segment': 'morris_chart'
-  }
-  return render(request, 'pages/chart-morris.html', context)
-
-@login_required(login_url='/accounts/login/')
-def google_maps(request):
-  context = {
-    'parent': 'maps',
-    'segment': 'google_maps'
-  }
-  return render(request, 'pages/map-google.html', context)
-
-# Authentication
-class UserRegistrationView(CreateView):
-  template_name = 'accounts/auth-signup.html'
-  form_class = RegistrationForm
-  success_url = '/accounts/login/'
-
-class UserLoginView(LoginView):
-  template_name = 'accounts/auth-signin.html'
-  form_class = LoginForm
-
-class UserPasswordResetView(PasswordResetView):
-  template_name = 'accounts/auth-reset-password.html'
-  form_class = UserPasswordResetForm
-
-class UserPasswrodResetConfirmView(PasswordResetConfirmView):
-  template_name = 'accounts/auth-password-reset-confirm.html'
-  form_class = UserSetPasswordForm
-
-class UserPasswordChangeView(PasswordChangeView):
-  template_name = 'accounts/auth-change-password.html'
-  form_class = UserPasswordChangeForm
-
-def logout_view(request):
-  logout(request)
-  return redirect('/accounts/login/')
-
-@login_required(login_url='/accounts/login/')
-def profile(request):
-  context = {
-    'segment': 'profile',
-  }
-  return render(request, 'pages/profile.html', context)
-
-@login_required(login_url='/accounts/login/')
 def sample_page(request):
   context = {
     'segment': 'sample_page',
   }
   return render(request, 'pages/sample-page.html', context)
+
+# Authentication
+class UserLoginView(LoginView):
+  template_name = 'accounts/login.html'
+  form_class = LoginForm
+
+def register(request):
+  if request.method == 'POST':
+    form = RegistrationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      print('Account created successfully!')
+      return redirect('/accounts/login/')
+    else:
+      print("Register failed!")
+  else:
+    form = RegistrationForm()
+
+  context = { 'form': form }
+  return render(request, 'accounts/register.html', context)
+
+def logout_view(request):
+  logout(request)
+  return redirect('/accounts/login/')
+
+class UserPasswordResetView(PasswordResetView):
+  template_name = 'accounts/password_reset.html'
+  form_class = UserPasswordResetForm
+
+class UserPasswordResetConfirmView(PasswordResetConfirmView):
+  template_name = 'accounts/password_reset_confirm.html'
+  form_class = UserSetPasswordForm
+
+class UserPasswordChangeView(PasswordChangeView):
+  template_name = 'accounts/password_change.html'
+  form_class = UserPasswordChangeForm
